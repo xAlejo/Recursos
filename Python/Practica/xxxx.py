0568d1ce817c66b44be9659fe20e5e2fd52e6d5e
AleB <alejoburgos987@gmail.com>
2026-04-20 10:26:43 -0400

    Update xxxx.py

--- Python/Practica/xxxx.py
+++ Python/Practica/xxxx.py
@@ -1,1 +1,27 @@
+# Ejercicios Propios
+```python
+# 1
+# Calculadora simple
 
+num1 = float(input("Primer número: "))
+operador = input("Operador (+, -, *, /): ")
+num2 = float(input("Segundo número: "))
+
+if operador == "+":
+    print("Resultado:", num1 + num2)
+
+elif operador == "-":
+    print("Resultado:", num1 - num2)
+
+elif operador == "*":
+    print("Resultado:", num1 * num2)
+
+elif operador == "/":
+    if num2 != 0:
+        print("Resultado:", num1 / num2)
+    else:
+        print("No se puede dividir por cero")
+
+else:
+    print("Operador no válido")
+```
