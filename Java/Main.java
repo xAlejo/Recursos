c7566df2f048db94b99ef69c55fa918a8b34eb9b
AleB <alejoburgos987@gmail.com>
2026-05-06 04:32:37 -0400

    Implement main method for Mascota class demo
    Added main method to demonstrate Mascota class functionality.

--- Java/Main.java
+++ Java/Main.java
@@ -1,1 +1,23 @@
 
+public class Main {
+    public static void main(String[] args) {
+
+        Mascota mascota1 = new Mascota("Miyu", 50, 20);
+        Mascota mascota2 = new Mascota("Nina", 30, 10);
+
+        mascota1.mostrarEstado();
+        mascota1.jugar();
+        mascota1.comer();
+        mascota1.dormir();
+
+        System.out.println("Energía actual: " + mascota1.getEnergia());
+
+        mascota2.mostrarEstado();
+        mascota2.setEnergia(40);
+        mascota2.jugar();
+        mascota2.comer();
+        mascota2.dormir();
+
+        System.out.println("Energía actual: " + mascota2.getEnergia());
+    }
+}
